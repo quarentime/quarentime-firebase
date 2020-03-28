8a87191407a5bec9e0cbe636be2a6401969a0a4f
Edmar <macbook@MacBook-Pro-Edmar.hub>
2020-03-28 19:24:43 +1300

    Contact tracing

--- hosting/functions/index.js
+++ hosting/functions/index.js
@@ -1,23 +1,101 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const datamodel = require('./statuschangematrix').datamodel;
 
 admin.initializeApp(functions.config().firebase);
 
 // Register existing user on firestore
 exports.createUser = functions.auth.user().onCreate((user) => {
     const db = admin.firestore();
-    let docRef = db.collection('users').doc(user.uid);
+    let docRef = db.collection('User').doc(user.uid);
 
     docRef.set(
         {
             'id': user.uid,
-            'email': user.email,
-            'name': user.displayName,
-            'days_in_quarantine': 0,
-            'days_in_post_quarantine': 0
-        }
-    )
-    .catch((err) => {
-        console.log(err);
-    })
+            'email': user.email
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
+
+// When the user status change, update contacts to reflect changes
+exports.updateContactsOnStatusChanges = functions.firestore.document('/User/{userId}')
+    .onUpdate(async (change, context) => {
+        const oldValue = change.before.data();
+        const newValue = change.after.data();
+        const updatedUserId = context.params.userId;
+
+        console.log(newValue);
+
+        if (oldValue.FinalStatus == newValue.FinalStatus) {
+            console.log('Statuses are the same. Doing nothing');
+            return;
+        }
+
+        try {
+            console.log('Statuses are different. Updating all the things');
+
+            const db = admin.firestore();
+            var snapshot = await db.collection('User')
+                .doc(updatedUserId)
+                .collection('Contacts')
+                .get();
+
+            snapshot.forEach(async (doc) => {
+                // Update contact on target user collection
+                await db.collection('User')
+                    .doc(doc.id)
+                    .collection('Contacts')
+                    .doc(updatedUserId)
+                    .update({ Status: newValue.FinalStatus });
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+// When the status of a contact change... (or a new contact is added)
+exports.changeStatusBasedOnContactCreate = functions.firestore.document('/User/{userId}/Contacts/{contactId}')
+    .onCreate(async (snap, context) => {
+        const newValue = snap.data();
+
+        console.log(`Contact ${newValue.Name} creatd with status: ${newValue.Status}`);
+    });
+
+exports.changeStatusBasedOnContactUpdate = functions.firestore.document('/User/{userId}/Contacts/{contactId}')
+    .onUpdate(async (change, context) => {
+        const oldValue = change.before.data();
+        const newValue = change.after.data();
+        const userIdToUpdate = context.params.userId;
+
+        if (oldValue.Status == newValue.Status) {
+            return;
+        }
+
+        try {
+
+            const db = admin.firestore();
+
+            var snapshot = await db.collection('User')
+                .doc(userIdToUpdate).get();
+
+            userToUpdate = snapshot.data();
+
+            var newStatus = datamodel[userToUpdate.Status][newValue.Status];
+
+            // We don't want to trigger unecessary updates
+            if (newStatus == newValue.Status) {
+                return;
+            }
+
+            await db.collection('User')
+                .doc(userIdToUpdate).update({ FinalStatus: newStatus });
+
+        } catch (error) {
+            console.log(error);
+        }
+
+        console.log(`Contact ${newValue.Name} has changed status to: ${newValue.Status}`);
+    });
---
+++ hosting/functions/statuschangematrix.js
@@ -0,0 +1,66 @@
+exports.datamodel = {
+    "Healthy": {
+        "Healthy": "Healthy",
+        "HealtySocialDistancing": "HealtySocialDistancing",
+        "LowProbabilitySuspected": "LowProbabilitySuspected",
+        "HighProbabilitySuspected": "HighProbabilitySuspected",
+        "FluLike": "Healthy",
+        "Positive": "HighProbabilitySuspected",
+        "Recovered": "Healthy"
+    },
+    "HealtySocialDistancing": {
+        "Healthy": "HealtySocialDistancing",
+        "HealtySocialDistancing": "HealtySocialDistancing",
+        "LowProbabilitySuspected": "LowProbabilitySuspected",
+        "HighProbabilitySuspected": "HighProbabilitySuspected",
+        "FluLike": "HealtySocialDistancing",
+        "Positive": "Positive",
+        "Recovered": "HealtySocialDistancing"
+    },
+    "LowProbabilitySuspected": {
+        "Healthy": "LowProbabilitySuspected",
+        "HealtySocialDistancing": "LowProbabilitySuspected",
+        "LowProbabilitySuspected": "LowProbabilitySuspected",
+        "HighProbabilitySuspected": "HighProbabilitySuspected",
+        "FluLike": "LowProbabilitySuspected",
+        "Positive": "HighProbabilitySuspected",
+        "Recovered": "LowProbabilitySuspected"
+    },
+    "HighProbabilitySuspected": {
+        "Healthy": "HighProbabilitySuspected",
+        "HealtySocialDistancing": "HighProbabilitySuspected",
+        "LowProbabilitySuspected": "HighProbabilitySuspected",
+        "HighProbabilitySuspected": "HighProbabilitySuspected",
+        "FluLike": "HighProbabilitySuspected",
+        "Positive": "HighProbabilitySuspected",
+        "Recovered": "HighProbabilitySuspected"
+    },
+    "FluLike": {
+        "Healthy": "FluLike",
+        "HealtySocialDistancing": "FluLike",
+        "LowProbabilitySuspected": "FluLike",
+        "HighProbabilitySuspected": "FluLike",
+        "FluLike": "FluLike",
+        "Positive": "FluLike",
+        "Recovered": "FluLike"
+    },
+    "Positive": {
+        "Healthy": "Positive",
+        "HealtySocialDistancing": "Positive",
+        "LowProbabilitySuspected": "Positive",
+        "HighProbabilitySuspected": "Positive",
+        "FluLike": "Positive",
+        "Positive": "Positive",
+        "Recovered": "Positive"
+    },
+    "Recovered": {
+        "Healthy": "Recovered",
+        "HealtySocialDistancing": "Recovered",
+        "LowProbabilitySuspected": "Recovered",
+        "HighProbabilitySuspected": "Recovered",
+        "FluLike": "Recovered",
+        "Positive": "Recovered",
+        "Recovered": "Recovered"
+    }
+
+};
